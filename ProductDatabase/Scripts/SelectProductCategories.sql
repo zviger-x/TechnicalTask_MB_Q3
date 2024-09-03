74794530f35695c16ff59b1e9723bd6a8fb1ae80
AlexeyMarkevich <zviger.alexey@gmail.com>
2024-09-03 18:49:37 +0300

    Added query

---
+++ ProductDatabase/Scripts/SelectProductCategories.sql
@@ -0,0 +1,4 @@
+﻿SELECT p.ProductName AS ProductName, ISNULL(c.CategoryName, 'No Category') AS CategoryName
+FROM [Products] p
+LEFT JOIN [ProductCategories] pc ON p.ProductId = pc.ProductId
+LEFT JOIN [Categories] c ON pc.CategoryId = c.CategoryId;
